e2502ae7ee7aabf35cecb9ce496db2b0528559f9
Cooper Jones <Cooper Jones>
2016-07-31 00:18:58 -0400

    removed all tools, added azimuthal orbit

--- loadMap.js
+++ loadMap.js
@@ -1,7 +1,25 @@
+function addNewTools(clara){
+  ['orbit', 'pan', 'zoom', 'home', 'fullscreen'].forEach(function(tool){
+    clara.player.removeTool(tool);
+  });
+  clara.player.addTool({
+    drag: function(){
+      return {
+        momentum:false,
+        handle: function(ev){
+          var nav = clara._store.getNavigator();
+          if(nav) nav.updateSphericalRotation({x:ev.deltaX, y:0});
+        }
+      }
+    }
+  },'azimuthRotate');
+}
+
 function loadMap(el, mapId){
   var clara = claraplayer(el);
   clara.sceneIO.fetchAndUse(mapId).then(function(){
     console.log('loaded');
+    addNewTools(clara);
   });
   console.log(clara._store);
 };

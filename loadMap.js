2ebb9b26fda3c1d0af2c165414da7fa3c8116433
Cooper Jones <Cooper Jones>
2016-07-30 21:07:45 -0400

    Changes to scene, closes #1

--- loadMap.js
+++ loadMap.js
@@ -1,7 +1,9 @@
 function loadMap(el, mapId){
   var clara = claraplayer(el);
   clara.sceneIO.fetchAndUse(mapId).then(function(){
+    console.log('loaded');
   });
+  console.log(clara._store);
 };
 
 loadMap('player', '8b99b7f6-8be9-4863-9dcd-4e73fde661ea');

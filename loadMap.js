228fc5b66dfa7eaf42bf83fe902bac94ec501b61
Cooper Jones <cooperjones@cmail.carleton.ca>
2016-07-31 14:12:27 -0400

    added slection tools close #2

--- loadMap.js
+++ loadMap.js
@@ -5,6 +5,26 @@ function addNewTools(clara){
   ['orbit', 'pan', 'zoom'].forEach(function(tool){
     clara.player.hideTool(tool);
   });
+  clara.player.addTool({
+    click:function(ev){
+      console.log(ev);
+      var THREE = clara.deps.THREE;
+      var bb = clara._store.getState().get('player').get('rect');
+      var ndc = new THREE.Vector2(
+        2*ev.clientX/bb.get('width')-1,
+        -2*ev.clientY/bb.get('height')+1
+      );
+      var hits = clara._store.getTranslator().raycastSelect(ndc);
+      console.log(hits);
+      var planeHit = hits.find(function(hit){
+        return hit.data[0].object.name==="Mat Floor";
+      });
+      var raw = planeHit.data[0].point;
+      var coord = new THREE.Vector3(Math.round(raw.x),0,Math.round(raw.z));
+      console.log(raw,coord);
+      clara.scene.set({name:'Sphere', plug:'Transform', property:'translation'}, coord);
+    }
+  },'select');
 }
 
 function loadMap(el, mapId){
